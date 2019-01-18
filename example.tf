32f915beabfc01bcc09be87dcde7053cdbcadc16
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-17 23:57:45 -0800

    Add security group to allow only inbound ssh

--- example.tf
+++ example.tf
@@ -3,9 +3,27 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+# Security group that allows only port 22 (SSH) for inbound and nothing for outbound 
+resource "aws_security_group" "ssh" {
+  name = "allow-ssh"
+  description = "Security group that allows SSH from internet"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow-ssh"
+  }
+}
+
 # The EC2 instance to be launched 
 resource "aws_instance" "example" {
   ami           = "ami-03fa1f014b48fa6bd"
   instance_type = "t2.micro"
   key_name        = "${var.keypair_name}"
+	vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
 }
