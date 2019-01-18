58c27b23c452dc6c6fbd9abb3747cf3ed72c90e0
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-17 16:18:44 -0800

    Add comments

--- example.tf
+++ example.tf
@@ -1,12 +1,15 @@
+# Define provider and region. Credentials are accessed from awscli default location. 
 provider "aws" {
   region     = "us-west-2"
 }
 
+# The EC2 instance to be launched 
 resource "aws_instance" "example" {
   ami           = "ami-03fa1f014b48fa6bd"
   instance_type = "t2.micro"
 }
 
+# Elastic IP for the above mentioned EC2 instance 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
 }
