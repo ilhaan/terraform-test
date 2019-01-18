5b312e99657d3737e48629a86fc4a381cb6a49fb
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-17 16:15:55 -0800

    Add elastic IP to ec2 instance

--- example.tf
+++ example.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "example" {
   ami           = "ami-03fa1f014b48fa6bd"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
