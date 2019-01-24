60c1d2aa1fca38d4288bc8592337f268a8eb66db
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-24 14:57:20 -0800

    Add variables files from ec2 test dir and define cluster name

---
+++ eks_cluster/variables.tf
@@ -0,0 +1,16 @@
+#variable "access_key" {}
+#variable "secret_key" {}
+# Above values are handled by awscli 
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "keypair_name" {
+	description = "The name of your pre-made key-pair in Amazon (e.g. david-IAM-keypair )"
+}
+
+variable "cluster-name" {
+  default = "ilhaan-terraform-eks-demo"
+  type    = "string"
+}
