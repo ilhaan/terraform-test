9c1629f391ce7afdab3c3355cb8cea968edccea3
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-24 16:17:25 -0800

    Add EKS master cluster

--- eks_cluster/example.tf
+++ eks_cluster/example.tf
@@ -117,3 +117,20 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   to_port           = 443
   type              = "ingress"
 }
+
+# ===================== EKS Master Cluster ====================================
+
+resource "aws_eks_cluster" "demo" {
+  name            = "${var.cluster-name}"
+  role_arn        = "${aws_iam_role.demo-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
+    subnet_ids         = ["${aws_subnet.demo.*.id}"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
+  ]
+}
