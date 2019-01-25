91e4a749fccd5eb3cd9b59049e99350c11a0283c
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-24 18:03:08 -0800

    Add sg for worker node to access EKS master cluster

--- eks_cluster/example.tf
+++ eks_cluster/example.tf
@@ -219,3 +219,15 @@ resource "aws_security_group_rule" "demo-node-ingress-cluster" {
   to_port                  = 65535
   type                     = "ingress"
 }
+
+# =============== Worker Node Access to EKS Master Cluster ====================
+
+resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.demo-cluster.id}"
+  source_security_group_id = "${aws_security_group.demo-node.id}"
+  to_port                  = 443
+  type                     = "ingress"
+}
