dd3bcef793c6dcbe0524c43b6f71fb9bf3366bf1
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-01-24 18:19:58 -0800

    Add AWS Auth configs required to allow nodes to connect to master

--- eks_cluster/outputs.tf
+++ eks_cluster/outputs.tf
@@ -27,8 +27,29 @@ users:
         - "-i"
         - "${var.cluster-name}"
 KUBECONFIG
+
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+name: aws-auth
+namespace: kube-system
+data:
+mapRoles: |
+  - rolearn: ${aws_iam_role.demo-node.arn}
+    username: system:node:{{EC2PrivateDNSName}}
+    groups:
+      - system:bootstrappers
+      - system:nodes
+CONFIGMAPAWSAUTH
 }
 
 output "kubeconfig" {
   value = "${local.kubeconfig}"
+}
+
+output "config_map_aws_auth" {
+  value = "${local.config_map_aws_auth}"
 }
